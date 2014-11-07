eab6c1d175aca2b3e323eb07ea5f7d7832c5dce8
Aaron <amacy@kabam.com>
2014-11-07 13:02:46 -0800

    cleaning up solution to prob 1

--- src/001.clj
+++ src/001.clj
@@ -8,15 +8,6 @@
   (or (multiple-of-3? i) (multiple-of-5? i)))
 
 (defn multiples-of-3-and-5 [is]
-  (loop [payload '()
-         tail_is (seq is)]
-    (if tail_is
-      (if (multiple-of-3-or-5? (first tail_is))
-        (recur (cons (first tail_is) payload)
-               (next tail_is))
-        (recur (cons 0 payload)
-               (next tail_is)))
-      payload)))
-
+  (keep #(if(multiple-of-3-or-5? %) %) is))
 
 (reduce + (multiples-of-3-and-5 (range 1000)))
