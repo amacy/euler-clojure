5cfce91af18e67bf0fdc75f5a255144f7a4f4f3c
Aaron <amacy@kabam.com>
2014-11-08 10:29:29 -0800

    cleaning up problem 2

--- src/002.clj
+++ src/002.clj
@@ -1,12 +1,10 @@
 (defn fibonacci [max-number]
     (loop [payload '(2 1)
            i 3]
-      (let [last-i (first payload)
-            next-i (+ i last-i)]
+      (let [last-i (first payload)]
         (if (>= i max-number)
           payload
           (recur (cons i payload)
-                 next-i)
-            ))))
+                 (+ i last-i))))))
 
 (reduce + (filter even? (fibonacci 4000000)))
